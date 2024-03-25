b7caab9a557e3b230c19042eacb7da4dffe4048e
Todd Tanner <todd@spawndev.com>
2024-03-25 16:22:53 -0400

    Added Class option for adding class names to the genereated QRCode element

--- SpawnDev.Blazor.QRCodeRenderer/SvgQRCodeExtensions.cs
+++ SpawnDev.Blazor.QRCodeRenderer/SvgQRCodeExtensions.cs
@@ -12,6 +12,24 @@ namespace SpawnDev.Blazor.QRCodeRenderer
         /// Returns a QR code as SVG string with custom colors (in HEX syntax), optional quietzone and logo
         /// </summary>
         /// <param name="_this"></param>
+        /// <param name="attributes"></param>
+        /// <param name="pixelsPerModule"></param>
+        /// <param name="darkColorHex"></param>
+        /// <param name="lightColorHex"></param>
+        /// <param name="drawQuietZones"></param>
+        /// <param name="sizingMode"></param>
+        /// <param name="logo"></param>
+        /// <returns></returns>
+        public static string GetGraphic(this SvgQRCode _this, string attributes, int pixelsPerModule, string darkColorHex, string lightColorHex, bool drawQuietZones = true, SizingMode sizingMode = SizingMode.ViewBoxAttribute, SvgLogo? logo = null)
+        {
+            var svg = _this.GetGraphic(pixelsPerModule, darkColorHex, lightColorHex, drawQuietZones, sizingMode, logo);
+            return $"<svg {attributes}" + svg.Substring(svg.IndexOf(" "));
+        }
+        /// <summary>
+        /// Returns a QR code as SVG string with custom colors (in HEX syntax), optional quietzone and logo
+        /// </summary>
+        /// <param name="_this"></param>
+        /// <param name="classNames"></param>
         /// <param name="style"></param>
         /// <param name="pixelsPerModule"></param>
         /// <param name="darkColorHex"></param>
@@ -20,10 +38,10 @@ namespace SpawnDev.Blazor.QRCodeRenderer
         /// <param name="sizingMode"></param>
         /// <param name="logo"></param>
         /// <returns></returns>
-        public static string GetGraphic(this SvgQRCode _this, string style, int pixelsPerModule, string darkColorHex, string lightColorHex, bool drawQuietZones = true, SizingMode sizingMode = SizingMode.ViewBoxAttribute, SvgLogo? logo = null)
+        public static string GetGraphic(this SvgQRCode _this, string classNames, string style, int pixelsPerModule, string darkColorHex, string lightColorHex, bool drawQuietZones = true, SizingMode sizingMode = SizingMode.ViewBoxAttribute, SvgLogo? logo = null)
         {
-            var svg = _this.GetGraphic(pixelsPerModule, darkColorHex, lightColorHex, drawQuietZones, sizingMode, logo);
-            return $"<svg style=\"{style}\"" + svg.Substring(svg.IndexOf(" "));
+            var attributes = $"class=\"{classNames}\" style=\"{style}\"";
+            return _this.GetGraphic(attributes, pixelsPerModule,darkColorHex,lightColorHex,drawQuietZones, sizingMode, logo);
         }
     }
 }
